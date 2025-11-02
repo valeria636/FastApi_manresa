ed4859cfb6e057cfe6a62c423dcce680b18e015a
valeria636 <valeriamanresa636@gmail.com>
2025-11-02 22:00:55 +0100

    final

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
-import os
-from typing import List
 from fastapi import FastAPI, Depends
-from dotenv import load_dotenv
-from sqlalchemy.sql.functions import user
-from sqlmodel import create_engine, Session, SQLModel, select
-
-from database import get_session
+from sqlmodel import Session, select
+from database import engine, get_session
 from models import Product, ProductPublic, SQLModel
-
-from models import Product
-
-load_dotenv()
-
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
-
-
-SQLModel.metadata.create_all(engine)
-
 
 app = FastAPI()
 
+@app.on_event("startup")
+def on_startup():
+    SQLModel.metadata.create_all(engine)
 
 @app.post("/api/product")
 def create_product(product: Product, db: Session = Depends(get_session)):
@@ -33,7 +19,7 @@ def create_product(product: Product, db: Session = Depends(get_session)):
 @app.get("/api/product", response_model=list[ProductPublic])
 def read_products(db: Session = Depends(get_session)):
     products = db.exec(select(Product)).all()
-    return products
+    return products  # secret_code és sensible i no s’envia
 
 @app.get("/api/product/{product_id}", response_model=ProductPublic)
 def read_product_by_id(product_id: int, db: Session = Depends(get_session)):
@@ -45,9 +31,9 @@ def read_product_by_category(category: str, db: Session = Depends(get_session)):
     result = db.exec(select(Product).where(Product.category == category)).all()
     return result
 
+@app.delete("/api/product/delete/{product_id}")
 def delete_product(product_id: int, db: Session = Depends(get_session)):
     product = db.get(Product, product_id)
     db.delete(product)
     db.commit()
     return {"message": "Producte eliminat correctament"}
-
